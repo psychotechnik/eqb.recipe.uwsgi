9eabd878474ff161cbdab5f32c547adbed398f66
Philip Kalinsky <psychotechnik@gmail.com>
2012-03-12 21:26:34 -0400

    fill out setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,36 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+version = '0.0.1'
 
 setup(
     name = "eqb.recipe.uwsgi",
+    version = version,
+    description = """\
+            this is a recipe to build a uwsgi binary as well as the xml config file.
+            modified version of https://github.com/shaunsephton/shaunsephton.recipe.uwsgi
+
+    """,
+    classifiers=[
+    'Framework :: Buildout',
+    'Topic :: Software Development :: Build Tools',
+    'Development Status :: 5 - Production/Stable',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 2.5',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    ],      
+    package_dir={'': 'eqb/recipe/uwsgi'},
+    packages=find_packages('eqb/recipe/uwsgi'),
+    keywords='',
+    author='Philip Kalinsky',
+    author_email='philip.kalinsky@eloquentbits.com',
+    url='https://github.com/psychotechnik/eqb.recipe.uwsgi',
+    license='BSD',
+    zip_safe=False,
+    install_requires=[
+        'zc.buildout',
+    ],
     entry_points = {
-        'zc.buildout': ['uwsgi = eqb.recipe.uwsgi:UWSGI']
+        'zc.buildout': ['default = eqb.recipe.uwsgi:UWSGI']
     },
     )
